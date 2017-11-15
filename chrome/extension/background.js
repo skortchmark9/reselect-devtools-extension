7396e12d0f77b5a34ffab69f6b5bd27e10bc2bff
Samuel Kortchmar <Samuel.Kortchmar@spacex.com>
2017-11-15 13:31:57 -0800

    Link extension to docs

--- chrome/extension/background.js
+++ chrome/extension/background.js
@@ -1,29 +1,3 @@
-const bluebird = require('bluebird');
-
-global.Promise = bluebird;
-
-function promisifier(method) {
-  // return a function
-  return function promisified(...args) {
-    // which returns a promise
-    return new Promise((resolve) => {
-      args.push(resolve);
-      method.apply(this, args);
-    });
-  };
-}
-
-function promisifyAll(obj, list) {
-  list.forEach(api => bluebird.promisifyAll(obj[api], { promisifier }));
-}
-
-// let chrome extension api support Promise
-promisifyAll(chrome, [
-  'tabs',
-  'windows',
-  'browserAction',
-  'contextMenus'
-]);
-promisifyAll(chrome.storage, [
-  'local',
-]);
+chrome.browserAction.onClicked.addListener(() => {
+  window.open('https://github.com/skortchmark9/reselect-devtools-extension');
+});
