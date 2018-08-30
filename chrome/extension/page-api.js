b7fa4b25612a2ec4803d722b5f2affe23ae780aa
XuluWarrior <github@metaliving.plus.com>
2018-08-30 17:25:56 +0100

    Use JSON to avoid eval corruption Otherwise Immutable objects are displayed in their internal rather than human friendly form

--- chrome/extension/page-api.js
+++ chrome/extension/page-api.js
@@ -1,6 +1,7 @@
 function evalPromise(str) {
   return new Promise((resolve, reject) => {
-    chrome.devtools.inspectedWindow.eval(str, (result, err) => {
+    chrome.devtools.inspectedWindow.eval(str, (resultStr, err) => {
+      const result = JSON.parse(resultStr);
       if (err && err.isException) {
         console.error(err.value);
         reject(err.value);
@@ -15,12 +16,12 @@ export function checkSelector(id) {
   const str = `(function() {
     const __reselect_last_check = window.__RESELECT_TOOLS__.checkSelector('${id}');
     console.log(__reselect_last_check);
-    return __reselect_last_check;
+    return JSON.stringify(__reselect_last_check);
   })()`;
   return evalPromise(str);
 }
 
 export function selectorGraph() {
-  const str = 'window.__RESELECT_TOOLS__.selectorGraph()';
+  const str = 'JSON.stringify(window.__RESELECT_TOOLS__.selectorGraph())';
   return evalPromise(str);
 }
